4c846cb4682d64965f7eb5e055bd006261d708a9
termul488-ai <termul488@gmail.com>
2026-01-08 01:00:08 +0700

    Update tcp.py

--- tcp.py
+++ tcp.py
@@ -61,8 +61,8 @@ def initHeaders():
 	return headers
 
 def handleStatusCodes(status_code):
-	global request_counter
 	time.sleep(2)
+	global request_counter
 	print(f"\033[33m "+str(url)+"\033[38;5;1m runing\033[0m")
 	sys.stdout.write("\033[100m \033[40m\r%i Target\033[0m" % request_counter) 
 	print(f"\033[48;5;5m "+str(url)+"\033[38;5;7m runing\033[0m")
