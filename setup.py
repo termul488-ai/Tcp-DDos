684b36f0d8b76d60941122bb683dc0eaecd2f960
termul488-ai <termul488@gmail.com>
2026-01-07 19:35:52 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,8 @@ print("""[0] pip\n[1] pip3\nWhich one do you use?""")
 c = input(">>>: ")
 if c == "0":
     os.system("pip install django")
-    os.system("pip install validators")
     os.system("pip install fade")
 elif c == "1":
     os.system("pip3 install django")
-    os.system("pip3 install validators")
     os.system("pip3 install fade")
 print("Done.")
