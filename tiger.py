824fdef39ff2e9ff9f84e2a15886a46b47f7a781
termul488-ai <termul488@gmail.com>
2026-01-08 10:56:07 +0700

    Update tiger.py

--- tiger.py
+++ tiger.py
@@ -38,6 +38,17 @@ logo = """
 """
 faded_text = fade.fire(logo)
 print(faded_text)
+while attemps < 100:
+    username = input("\033[32mEnter your username: \033[0m")
+    password = input("\033[31mEnter your password: \033[0m")
+
+    if username == 'bp4' and password == 'bp4':
+        print("\033[32m⟩⟩ Hai...! Welcome to zona attack BLACKPHANTER \033[0m")
+        break
+    else:
+        print('Incorrect credentials. Check if you have Caps lock on and try again.')
+        attemps += 1
+        continue
 
 host=''
 headers_useragents=[]
